372905fe8c3d18c51fe5125ac5d29d88a34e6060
Unique01k <105670806+Unique01k@users.noreply.github.com>
2022-06-22 23:49:28 +0300

    Add files via upload

---
+++ JS_2.js
@@ -0,0 +1,18 @@
+
+const age2 = 18;
+const age3 = 60;
+
+const checkAge = function(age) {
+    if (age < age2){
+        console.log("You don't have access cause your age is " + age + " It's less then")  ; 
+}
+    if (age >= age2, age_1 < age3 ){
+        console.log("Welcome!");
+}
+    if (age > age3 ){
+        console.log("Keep calm and look Culture channel");
+}
+    else {
+        console.log("Technical work");
+}
+}
